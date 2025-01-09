b2b8dfc62131de67a8df438ef5f5d3f0491edc74
Enan Ajmain <3nan.ajmain@gmail.com>
2025-01-09 02:26:25 -0500

    evaluator: FGA,FTA implementations from loghub-2.0

--- src/evaluator.py
+++ src/evaluator.py
@@ -5,47 +5,34 @@ from nltk.metrics.distance import edit_distance as calc_edit_distance
 
 from settings import benchmark_settings
 
-pd.options.display.float_format = "{:,.2f}".format
-
-def get_accuracy(series_groundtruth, series_parsedlog, filter_templates=None):
+def get_accuracy(series_groundtruth, series_parsedlog):
     series_groundtruth_valuecounts = series_groundtruth.value_counts()
     series_parsedlog_valuecounts = series_parsedlog.value_counts()
     df_combined = pd.concat([series_groundtruth, series_parsedlog], axis=1, keys=['groundtruth', 'parsedlog'])
-    grouped_df = df_combined.groupby('groundtruth')
-    accurate_events = 0 # determine how many lines are correctly parsed
-    accurate_templates = 0
-    if filter_templates is not None:
-        filter_identify_templates = set()
-    # for ground_truthId in series_groundtruth_valuecounts.index:
-    for ground_truthId, group in grouped_df:
-        # logIds = series_groundtruth[series_groundtruth == ground_truthId].index
-        series_parsedlog_logId_valuecounts = group['parsedlog'].value_counts()
-        if filter_templates is not None and ground_truthId in filter_templates:
-            for parsed_eventId in series_parsedlog_logId_valuecounts.index:
-                filter_identify_templates.add(parsed_eventId)
-        if series_parsedlog_logId_valuecounts.size == 1:
-            parsed_eventId = series_parsedlog_logId_valuecounts.index[0]
-            if len(group) == series_parsedlog[series_parsedlog == parsed_eventId].size:
-                if (filter_templates is None) or (ground_truthId in filter_templates):
-                    accurate_events += len(group)
-                    accurate_templates += 1
-    # print("filter templates: ", len(filter_templates))
-    # print("total messages: ", len(series_groundtruth[series_groundtruth.isin(filter_templates)]))
-    # print("set of total: ", len(filter_identify_templates))
-    # print(accurate_events, accurate_templates)
-    if filter_templates is not None:
-        GA = float(accurate_events) / len(series_groundtruth[series_groundtruth.isin(filter_templates)])
-        PGA = float(accurate_templates) / len(filter_identify_templates)
-        RGA = float(accurate_templates) / len(filter_templates)
-    else:
-        GA = float(accurate_events) / len(series_groundtruth)
-        PGA = float(accurate_templates) / len(series_parsedlog_valuecounts)
-        RGA = float(accurate_templates) / len(series_groundtruth_valuecounts)
-    # print(FGA, RGA)
-    FGA = 0.0
-    if PGA != 0 or RGA != 0:
-        FGA = 2 * (PGA * RGA) / (PGA + RGA)
-    return GA, FGA, PGA, RGA
+
+    correctly_grouped_events = 0
+    correctly_grouped_templates = 0
+    correctly_parsed_templates = 0
+
+    for groundtruth_template, group in df_combined.groupby('groundtruth'):
+        parsed_template = list(group['parsedlog'].unique())
+        if len(parsed_template) == 1:
+            if len(group) == series_parsedlog[series_parsedlog == parsed_template[0]].size:
+                correctly_grouped_events += len(group)
+                correctly_grouped_templates += 1
+                if parsed_template[0] == groundtruth_template:
+                    correctly_parsed_templates += 1
+
+    GA = correctly_grouped_events / len(series_groundtruth)
+    PGA = float(correctly_grouped_templates) / len(series_parsedlog_valuecounts)
+    RGA = float(correctly_grouped_templates) / len(series_groundtruth_valuecounts)
+    PTA = float(correctly_parsed_templates) / len(series_parsedlog_valuecounts)
+    RTA = float(correctly_parsed_templates) / len(series_groundtruth_valuecounts)
+
+    FGA = 0.0 if PGA == 0 and RGA == 0 else 2 * (PGA * RGA) / (PGA + RGA)
+    FTA = 0.0 if PTA == 0 and RTA == 0 else 2 * (PTA * RTA) / (PTA + RTA)
+
+    return GA, FGA, FTA, PGA, RGA, PTA, RTA
 
 def get_PA(series_groundtruth, series_parsedlog):
     correctly_parsed_logs = series_groundtruth.eq(series_parsedlog).values.sum()
@@ -65,6 +52,8 @@ if __name__ == "__main__":
 
     import argparse
     from pathlib import Path
+    rootdir = Path(__file__).absolute().parent.parent
+    print(f"Project root: {rootdir}")
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--dirpath", type=str, default=None)
@@ -73,6 +62,7 @@ if __name__ == "__main__":
         "--projects",
         type=str,
         default="Android,Apache,BGL,HDFS,HPC,Hadoop,HealthApp,Linux,Mac,OpenSSH,OpenStack,Proxifier,Spark,Thunderbird,Windows,Zookeeper"
+        # default="Apache,BGL,HDFS,HPC,Hadoop,HealthApp,Linux,Mac,OpenSSH,OpenStack,Proxifier,Spark,Thunderbird,Zookeeper"
     )
     args = parser.parse_args()
 
@@ -80,31 +70,33 @@ if __name__ == "__main__":
     for project in args.projects.split(","):
         print(project)
 
-        predic_file = Path(args.dirpath) / project / "prediction.csv"
+        dataset_dir = rootdir / "2k_dataset"
+        predic_file = Path(args.dirpath) / project / "predictions.csv"
         result_file = Path(args.dirpath) / project / "result.csv"
-        df_parsedlog = pd.read_csv(
-            predic_file, index_col=False, header=None, names=["Log", "Predict", "EventTemplate"]
-        ).map(str)
-        f_result = open(result_file, "w")
-        print("Batch,GA,PA,ED", file=f_result)
 
-        results = []
+        df_parsedlog = pd.read_csv(predic_file, index_col=False, header='infer').map(str)
+        df_groundtruth = pd.read_csv(dataset_dir/project/(project+"_2k.log_structured_corrected.csv"))
+        series_parsedlog = df_parsedlog['EventTemplate']
+        series_groundtruth = df_groundtruth['EventTemplate']
+
+        f_result = open(result_file, "w")
+        print("Batch,   GA,    PA,   FGA,   FTA,    ED")
+        print("Batch,GA,PA,FGA,FTA,ED", file=f_result)
+
         edit_distance_in_batch = []
         for i in range(len(df_parsedlog)//batch_size):
-            df = df_parsedlog.iloc[:(i+1)*batch_size].copy()
-            series_parsedlog = df["Predict"].str.replace( r"\s+", "", regex=True)
-            series_groundtruth = df["EventTemplate"].str.replace( r"\s+", "", regex=True)
+            _series_parsedlog   = series_parsedlog.iloc[:(i+1)*batch_size].copy() #.str.replace( r"\s+", "", regex=True)
+            _series_groundtruth = series_groundtruth.iloc[:(i+1)*batch_size].copy() #.str.replace( r"\s+", "", regex=True)
 
-            GA, FGA, _,_ = get_accuracy(series_groundtruth, series_parsedlog)
-            PA = get_PA(series_groundtruth, series_parsedlog)
+            GA, FGA, FTA, _,_,_,_ = get_accuracy(_series_groundtruth, _series_parsedlog)
+            PA = get_PA(_series_groundtruth, _series_parsedlog)
 
-            edit_distance,_ = get_ED(series_groundtruth.tail(batch_size),
-                                     series_parsedlog.tail(batch_size))
+            edit_distance,_ = get_ED(_series_groundtruth.tail(batch_size),
+                                     _series_parsedlog.tail(batch_size))
             edit_distance_in_batch.append(edit_distance)
             ED = np.mean(edit_distance_in_batch)
 
-            results.append([i+1, GA,PA,ED])
-            print(f"{i+1:04d},{GA:.03f},{PA:.03f},{ED:.03f}")
-            print(f"{i+1:04d},{GA:.03f},{PA:.03f},{ED:.03f}", file=f_result)
+            print(f"{i+1:04d},{GA:.04f},{PA:.04f},{FGA:.04f},{FTA:.04f},{ED:.04f}")
+            print(f"{i+1:04d},{GA:.04f},{PA:.04f},{FGA:.04f},{FTA:.04f},{ED:.04f}", file=f_result)
 
         f_result.close()
